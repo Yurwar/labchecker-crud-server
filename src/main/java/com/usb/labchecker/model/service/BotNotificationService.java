42e113ff44bdcc779745742659254297c0eeca13
CodeGangsta44 <dovorman44@gmail.com>
2019-12-02 18:16:58 +0200

    fix: replace http request on https with noop ssl hostname verifier

--- src/main/java/com/usb/labchecker/model/service/BotNotificationService.java
+++ src/main/java/com/usb/labchecker/model/service/BotNotificationService.java
@@ -1,10 +1,17 @@
 package com.usb.labchecker.model.service;
 
-import com.usb.labchecker.model.dto.*;
+import com.usb.labchecker.model.dto.BotBuildFinishNotificationDto;
+import com.usb.labchecker.model.dto.BotBuildStartNotificationDto;
+import com.usb.labchecker.model.dto.LabResultTestingServerDto;
+import com.usb.labchecker.model.dto.TestRunnerBuildStartNotificationDto;
 import com.usb.labchecker.model.entity.Lab;
 import com.usb.labchecker.model.entity.Student;
 import com.usb.labchecker.model.repository.LabRepository;
 import com.usb.labchecker.model.repository.StudentRepository;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +19,8 @@ import java.util.NoSuchElementException;
 
 @Service
 public class BotNotificationService {
-    private static final String BUILD_START_ENDPOINT = "http://lab-checker-bot.maslovmikhail2000.now.sh/notification/build-start";
-    private static final String BUILD_FINISH_ENDPOINT = "http://lab-checker-bot.maslovmikhail2000.now.sh/notification/build-finish";
+    private static final String BUILD_START_ENDPOINT = "https://lab-checker-bot.maslovmikhail2000.now.sh/notification/build-start";
+    private static final String BUILD_FINISH_ENDPOINT = "https://lab-checker-bot.maslovmikhail2000.now.sh/notification/build-finish";
 
     private StudentRepository studentRepository;
     private LabRepository labRepository;
@@ -24,6 +31,24 @@ public class BotNotificationService {
         this.labRepository = labRepository;
     }
 
+    public static void main(String[] args) {
+        BotBuildStartNotificationDto botBuildStartNotificationDto = BotBuildStartNotificationDto.builder()
+                .studentId(1)
+                .subjectId(1)
+                .labId(1)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        restTemplate.postForLocation(BUILD_START_ENDPOINT, botBuildStartNotificationDto);
+    }
 
     public void notifyAboutStartOfBuild(TestRunnerBuildStartNotificationDto testRunnerBuildStartNotificationDto) {
 
@@ -35,14 +60,20 @@ public class BotNotificationService {
                 .findByRepoName(testRunnerBuildStartNotificationDto.getRepoName())
                 .orElseThrow(NoSuchElementException::new);
 
-
-        RestTemplate restTemplate = new RestTemplate();
-
         BotBuildStartNotificationDto botBuildStartNotificationDto = BotBuildStartNotificationDto.builder()
                 .studentId(student.getId())
                 .subjectId(lab.getCourse().getSubject().getId())
                 .labId(lab.getId())
                 .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
         restTemplate.postForLocation(BUILD_START_ENDPOINT, botBuildStartNotificationDto);
     }
@@ -56,14 +87,21 @@ public class BotNotificationService {
                 .findByRepoName(labResultDto.getRepositoryName())
                 .orElseThrow(NoSuchElementException::new);
 
-        RestTemplate restTemplate = new RestTemplate();
-
         BotBuildFinishNotificationDto botBuildFinishNotificationDto = BotBuildFinishNotificationDto.builder()
                 .studentId(student.getId())
                 .subjectId(lab.getCourse().getSubject().getId())
                 .labId(lab.getId())
                 .result(labResultDto.getMark())
                 .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
         restTemplate.postForLocation(BUILD_FINISH_ENDPOINT, botBuildFinishNotificationDto);
     }
